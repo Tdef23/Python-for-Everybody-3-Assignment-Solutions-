c2e31fc6b0419517483042a01d6ba462fdbdfce7
Bhat632 <tyler.wesley16@gmail.com>
2022-12-14 13:14:50 -0700

    last list week commit

---
+++ Lists_Week/Lists_Week_Ex6.py
@@ -0,0 +1,33 @@
+#Exercise 6: Rewrite the program that prompts the user 
+#for a list of numbers and prints out the maximum and 
+#minimum of the numbers at the end when the user enters 
+#"done". Write the program to store the numbers the user 
+#enters in a list and use the max() and min() functions 
+#to compute the maximum and minimum numbers after the loop 
+#completes. 
+
+#Enter a number: 6
+#Enter a number: 2
+#Enter a number: 9
+#Enter a number: 3
+#Enter a number: 5
+#Enter a number: done
+#Maximum: 9.0
+#Minimum: 2.0 
+
+new_list = []
+while(True):
+    num = input('Enter a number:')
+    if num == 'done': 
+        print('Enter a number:', num)
+        break
+    try:
+        float(num)
+    except:
+        print('Invalid Entry')
+        quit()
+    print('Enter a number:', num)
+    new_list.append(num)
+    
+print('Maximum:', float(max(new_list)))
+print('Minimum:', float(min(new_list)))
