c6941a8c0e58e681ade02b78cb09b66113fd58c3
Bhat632 <tyler.wesley16@gmail.com>
2023-01-03 11:53:53 -0700

    dict

--- Dict_Week/Dict_Week_Ex1.py
+++ Dict_Week/Dict_Week_Ex1.py
@@ -1,3 +1,7 @@
+#Exercise 1: Download a copy of the file www.py4e.com/code3/words.txt Write a program that reads the words in words.txt and stores 
+#them as keys in a dictionary. It doesn't matter what the values are. Then you can use the in operator as a fast way to check whether 
+#a string is in the dictionary.
+
 try:
     fname = input('Enter file name:')
     fhand = open(fname)
