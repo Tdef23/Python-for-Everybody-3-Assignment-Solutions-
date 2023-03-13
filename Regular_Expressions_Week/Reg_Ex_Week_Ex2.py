fa8fe425db92aa3f2d307d3bdebc1f26b6fb8227
Bhat632 <tyler.wesley16@gmail.com>
2023-03-13 17:45:03 -0600

    Ex2

---
+++ Regular_Expressions_Week/Reg_Ex_Week_Ex2.py
@@ -0,0 +1,35 @@
+#Exercise 2: Write a program to look for lines of the form: 
+
+#New Revision: 39772 
+
+#Extract the number from each of the lines using a regular expression and the findall() method. Compute the average of the numbers 
+#and print out the average. 
+
+#Enter file:mbox.txt
+38444.0323119
+
+#Enter file:mbox-short.txt
+39756.9259259
+
+import re
+
+fname = input('Enter file:')
+try:
+    fhand = open('fname')
+except:
+    print('File cannot be opened')
+    quit()
+
+nrSearch = '^New Revision: ([0-9.]+)' #establish expression code to find
+match = 0 #establish count
+sum = 0 #stablish sum
+
+for line in hand:
+    line = line.strip()
+    nr = re.findall(nrSearch, line) #expression code simplified
+    if len(nr) > 0:
+        sum = int(nr[0]) + sum #conduct sum
+        match += 1 #start match
+    
+average = sum/match
+print(average)
